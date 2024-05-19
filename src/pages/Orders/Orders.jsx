736589ec0968ebc91dee0bc7355010308482d2b8
akash202004 <akashlaha48@gmail.com>
2024-05-19 12:10:39 +0530

    Refactor Orders component and add order fetching functionality

--- src/pages/Orders/Orders.jsx
+++ src/pages/Orders/Orders.jsx
@@ -1,7 +1,94 @@
+import { useState } from "react";
 import "./Orders.css";
+import { toast } from "react-toastify";
+import { useEffect } from "react";
+import axios from "axios";
+import { assets } from "../../assets/assets";
 
 const Orders = ({ url }) => {
-  return <div>Orders</div>;
+  const [orders, setOrders] = useState([]);
+  const fetchAllOrders = async () => {
+    try {
+      const response = await axios.get(url + "/api/order/listorder");
+      if (response.data.success) {
+        setOrders(response.data.data);
+        toast.success("Orders fetched successfully");
+      } else {
+        toast.error("Erorr while fetching orders");
+      }
+    } catch (error) {
+      toast.error("Erorr while fetching orders");
+    }
+  };
+
+  const formatDate = (dateString) => {
+    const options = {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+    };
+    return new Date(dateString).toLocaleDateString(undefined, options);
+  };
+
+  useEffect(() => {
+    fetchAllOrders();
+  }, []);
+
+  return (
+    <div className="order add">
+      <h3>Order Page</h3>
+      <div className="order-list">
+        {orders.length > 0 ? (
+          orders.map((order, index) => (
+            <div key={index} className="order-item">
+              <img src={assets.parcel_icon} alt="parcel-icon" />
+              <div>
+                <p className="order-item-food">
+                  {order.items.map((item, index) => {
+                    if (index === order.items.length - 1) {
+                      return item.name + " x " + item.quantity;
+                    } else {
+                      return item.name + " x " + item.quantity + ", ";
+                    }
+                  })}
+                </p>
+                <p className="order-item-name">
+                  {order.address.firstName + " " + order.address.lastName}
+                </p>
+                <div className="order-item-address">
+                  <p>{order.address.street + ","}</p>
+                  <p>
+                    {order.address.city +
+                      ", " +
+                      order.address.state +
+                      ", " +
+                      order.address.state +
+                      ", " +
+                      order.address.country +
+                      ", " +
+                      order.address.zipcode}
+                  </p>
+                </div>
+                <p className="order-item-phone">{order.address.phone}</p>
+              </div>
+              <p>Items: {order.items.length}</p>
+              <p>${order.amount}</p>
+              <p>{formatDate(order.date)}</p>
+              <select>
+                <option value="Food Processing">Food Processing</option>
+                <option value="Out For Delivery">Out For Delivery</option>
+                <option value="Delivered">Delivered</option>
+              </select>
+            </div>
+          ))
+        ) : (
+          <h3>No Orders</h3>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Orders;
