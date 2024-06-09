348c54e836bbff3f8676bd73e7233dc9c79d4db2
akash202004 <akashlaha48@gmail.com>
2024-06-09 20:56:24 +0530

    Add .env file and update API URL

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
-  const url = " http://localhost:4000";
+  const url = import.meta.env.VITE_APP_URL;
 
   return (
     <div>
